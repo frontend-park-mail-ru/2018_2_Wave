30a496401da1bca2b58f8184efe27eeb9fd2fd8a
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-09-24 11:15:25 +0300

    small style improvements

--- serv.js
+++ serv.js
@@ -16,70 +16,70 @@ app.use(cookie());
 
 
 const users = {
-	"1" : {
-        name : "1",
-        email : "1@mail.ru",
-        password : "111111",
-    },
-    "2" : {
-        name : "2",
-        email : "2@mail.ru",
-        password : "222222",
-    },
-    "3" : {
-        name : "3",
-        email : "3@mail.ru",
-        password : "333333",
-    },
+  "1" : {
+    name : "1",
+    email : "1@mail.ru",
+    password : "111111",
+  },
+  "2" : {
+    name : "2",
+    email : "2@mail.ru",
+    password : "222222",
+  },
+  "3" : {
+    name : "3",
+    email : "3@mail.ru",
+    password : "333333",
+  },
 };
 
 const ids = {};
 
 app.post('/signup', function (req, res) {
-    const email = req.body.name;
-    const email = req.body.email;
-	const password = req.body.password;
+  const email = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
 
-	if (users[email]) {
-		return res.status(400).json({answer: "fail"}); // user already exists
-	}
+  if (users[email]) {
+    return res.status(400).json({answer: "fail"}); // user already exists
+  }
 
-	const id = uuid();
-	const user = {name, email, password};
-	ids[id] = email;
-	users[email] = user;
+  const id = uuid();
+  const user = {name, email, password};
+  ids[id] = email;
+  users[email] = user;
 
-	res.cookie('sessionid', id, {expires: new Date(Date.now() + 1000 * 60 * 10)});
-	res.status(201).json({id});
+  res.cookie('sessionid', id, {expires: new Date(Date.now() + 1000 * 60 * 10)});
+  res.status(201).json({id});
 });
 
 app.post('/login', function (req, res) {
-	const password = req.body.email;
-    const email = req.body.password;
-    
-	if (!users[email] || users[email].password !== password) {
-		return res.status(400).json({answer : "fail"}); // wrong email or/and password
-	}
+  const password = req.body.email;
+  const email = req.body.password;
 
-	const id = uuid();
-	ids[id] = email;
+  if (!users[email] || users[email].password !== password) {
+    return res.status(400).json({answer : "fail"}); // wrong email or/and password
+  }
 
-	res.cookie('sessionid', id, {expires: new Date(Date.now() + 1000 * 60 * 10)});
-	res.status(201).json({id});
+  const id = uuid();
+  ids[id] = email;
+
+  res.cookie('sessionid', id, {expires: new Date(Date.now() + 1000 * 60 * 10)});
+  res.status(201).json({id});
 });
 
 app.get('/me', function (req, res) {
-	const id = req.cookies['sessionid'];
-	const email = ids[id];
-	if (!email || !users[email]) {
-		return res.status(401).end();
-	}
+  const id = req.cookies.sessionid;
+  const email = ids[id];
+  if (!email || !users[email]) {
+    return res.status(401).end();
+  }
 
-	res.json(users[email]);
+  res.json(users[email]);
 });
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, function () {
-	console.log(`Server listening port ${port}`);
+  console.log(`Server listening port ${port}`);
 });
