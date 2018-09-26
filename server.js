674ca1bbef4861f3c894323bc17e80fe0daec360
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-09-26 10:48:37 +0300

    removed JShint, added and configured ESLint, many style fixes

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
-'use strict';
-
 // const cookie = require('cookie-parser');
 
 const morgan = require('morgan');
 const express = require('express');
 const favicon = require('serve-favicon');
 const uuid = require('uuid/v4');
+const path = require('path');
+
 const app = express();
-
-
-app.use(morgan('dev'));
-app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/dist'));
-app.use(favicon(__dirname + '/public/img/favicon.png'));
+app.use(morgan('dev'));  // logging
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/dist')));
+app.use(favicon(path.join(__dirname, '/public/img/favicon.png')));
 
 
 // app.use(body.json());
@@ -22,74 +20,74 @@ app.use(favicon(__dirname + '/public/img/favicon.png'));
 const ids = {};
 
 const users = {
-  "1" : {
-    name : "1",
-    email : "1@mail.ru",
-    password : "111111",
+  1: {
+    name: '1',
+    email: '1@mail.ru',
+    password: '111111',
   },
-  "2" : {
-    name : "2",
-    email : "2@mail.ru",
-    password : "222222",
+  2: {
+    name: '2',
+    email: '2@mail.ru',
+    password: '222222',
   },
-  "3" : {
-    name : "3",
-    email : "3@mail.ru",
-    password : "333333",
+  3: {
+    name: '3',
+    email: '3@mail.ru',
+    password: '333333',
   },
 };
 
 
-app.post('/signup', function (req, res) {
-  const name = req.body.name;
-  const email = req.body.email;
-  const password = req.body.password;
+app.post('/signup', (req, res) => {
+  const { name } = req.body.name;
+  const { email } = req.body.email;
+  const { password } = req.body.password;
 
   if (users[email]) {
     // user already exists
-    return res.status(400).json({answer: "fail"});
+    return res.status(400).json({ answer: 'fail' });
   }
 
   const id = uuid();
-  const user = {name, email, password};
+  const user = { name, email, password };
   ids[id] = email;
   users[email] = user;
 
-  res.cookie('sessionid', id, {expires: new Date(Date.now() + 1000 * 60 * 10)});
-  res.status(201).json({id});
+  res.cookie('sessionid', id, { expires: new Date(Date.now() + 1000 * 60 * 10) });
+  return res.status(200).json({ id });
 });
 
 
-app.post('/login', function (req, res) {
-  const email = req.body.email;
-  const password = req.body.password;
+app.post('/login', (req, res) => {
+  const { email } = req.body.email;
+  const { password } = req.body.password;
 
   if (!users[email] || users[email].password !== password) {
     // wrong email or/and password
-    return res.status(400).json({answer : "fail"});
+    return res.status(400).json({ answer: 'fail' });
   }
 
   const id = uuid();
   ids[id] = email;
 
-  res.cookie('sessionid', id, {expires: new Date(Date.now() + 1000 * 60 * 10)});
-  res.status(201).json({id});
+  res.cookie('sessionid', id, { expires: new Date(Date.now() + 1000 * 60 * 10) });
+  return res.status(200).json({ id });
 });
 
 
-app.get('/me', function (req, res) {
+app.get('/me', (req, res) => {
   const id = req.cookies.sessionid;
   const email = ids[id];
   if (!email || !users[email]) {
     return res.status(401).end();
   }
 
-  res.json(users[email]);
+  return res.status(200).json(users[email]);
 });
 
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, function () {
+app.listen(port, () => {
   console.log(`Server listening port ${port}`);
 });
