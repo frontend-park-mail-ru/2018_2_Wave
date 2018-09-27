30e6055da7cfae5812b78f6dae5e98bf8b7ca910
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-09-27 17:49:29 +0300

    Unique click event listener added

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,40 @@
-import createMenu from './components/menu/menu/menu';
+import createMenu from './components/menu/menu';
+import createLogin from './components/login/login';
+import createRegister from './components/register/register';
+import createLeaderboard from './components/leaderboard/leaderboard';
+import createProfile from './components/profile/profile';
+import createSettings from './components/settings/settings';
+
 
 import css from '../css/style.css';
 
 const root = document.getElementById('root');
+
+
+const pages = {
+  play: createMenu,
+  menu: createMenu,
+  login: createLogin,
+  register: createRegister,
+  leaderboard: createLeaderboard,
+  profile: createProfile,
+  settings: createSettings,
+};
+
+
+root.addEventListener('click', (event) => {
+  if (!(event.target instanceof HTMLAnchorElement)) {
+    return;
+  }
+
+  event.preventDefault();
+  const link = event.target;
+
+  console.log(link);
+  console.log(link.datahref);
+
+  root.innerHTML = '';
+  pages[link.getAttribute('datahref')]();
+});
 
 createMenu();
