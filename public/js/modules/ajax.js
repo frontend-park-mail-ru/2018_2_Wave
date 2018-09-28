06e9b61937a40ea98fd441b4a0ca0d0d9d8af232
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-09-28 06:45:57 +0300

    + ajax module

--- public/js/modules/ajax.js
+++ public/js/modules/ajax.js
@@ -1,44 +1,47 @@
 (() => {
   const noop = () => null;
 
-  class AjaxModule {
-    static _ajax({
-      callback = noop,
-      method = 'GET',
-      path = '/', body,
-    } = {}) {
-      const xhr = new XMLHttpRequest();
-      xhr.open(method, path, true);
-      xhr.withCredentials = true;
+  function ajax({
+    callback = noop,
+    method = 'GET',
+    path = '/',
+    body,
+  } = {}) {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, path, true);
+    xhr.withCredentials = true;
 
-      if (body) {
-        xhr.setRequestHeader(
-          'Content-Type', 'application/json; charset=utf-8',
-        );
-      }
-
-      xhr.onreadystatechange = () => {
-        if (xhr.readyState !== 4) {
-          return;
-        }
-        callback(xhr);
-      };
-
-      if (body) {
-        xhr.send(JSON.stringify(body));
-      } else {
-        xhr.send();
-      }
+    if (body) {
+      xhr.setRequestHeader(
+        'Content-Type', 'application/json; charset=utf-8',
+      );
     }
 
-    doGet(params = {}) {
-      this.ajax({ ...params, method: 'GET' });
-    }
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== 4) {
+        return;
+      }
+      callback(xhr);
+    };
 
-    doPost(params = {}) {
-      this.ajax({ ...params, method: 'POST' });
+    if (body) {
+      xhr.send(JSON.stringify(body));
+    } else {
+      xhr.send();
     }
   }
 
-  window.AjaxModule = new AjaxModule();
+  function doGet(params = {}) {
+    ajax({ ...params, method: 'GET' });
+  }
+
+  function doPost(params = {}) {
+    ajax({ ...params, method: 'POST' });
+  }
+
+
+  window.AjaxModule = {
+    doGet,
+    doPost,
+  };
 })();
