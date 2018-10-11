0cd596af1ced202a0f57d5db8e7a6bfac18f879d
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-10-12 01:30:25 +0300

    new cool secure network module! Uses fetch API and bus module

--- src/modules/ajax.js
+++ src/modules/ajax.js
@@ -1,21 +1,64 @@
-const noop = () => null;
+const { bus } = window;
+
+
+const URL = 'https://rasseki.com';
+
+// errors which are handled by another modules
+const errorEvents = {
+  401: 'unauthorized',
+  500: 'serverError',
+};
+
+function getCodeType(statusCode) {
+  let codeType;
+  switch (parseInt(statusCode / 100, 10)) {
+    case 1:  codeType = 'Informational'; break;
+    case 2:  codeType = 'Success';       break;
+    case 3:  codeType = 'Redirection';   break;
+    case 4:  codeType = 'Client Error';  break;
+    case 5:  codeType = 'Server Error';  break;
+    default: codeType = 'Unknown';       break;
+  }
+  return codeType;
+}
+
 
 function ajax({
-  callback = noop,
   method = 'GET',
   path = '/',
   body,
+  callback = {
+    success: () => null,
+    failure: () => null,
+  },
 } = {}) {
-  // когда у нас появятся данные не в формах
-  if (!(body instanceof FormData)) {
-    body = JSON.stringify(body);
-  }
-  fetch(`https://rasseki.com${path}`, {
+  const URI = URL + path;
+  const params = {
     method,
-    body,
+    body: (body instanceof FormData) ? body : JSON.stringify(body),
     mode: 'cors',
     credentials: 'include',
-  }).then(callback);
+  };
+
+  fetch(URI, params)
+    .then((response) => {
+      const { status } = response;
+      const codeType = getCodeType(status);
+
+      if (codeType === 'Server Error'
+      ||  codeType === 'Client Error'
+      ||  codeType === 'Unknown') {
+        const errorEvent = errorEvents[status];
+        const error = { status, codeType, errorEvent };
+        if (errorEvent) bus.emit(errorEvent);
+        throw error;
+      } else {
+        return response.text();
+      }
+    })
+    .then(text => (text ? JSON.parse(text) : {}))
+    .then(data => callback.success(data))
+    .catch(error => callback.failure(error));
 }
 
 function Get(params = {}) {
@@ -30,15 +73,11 @@ function Put(params = {}) {
   ajax({ ...params, method: 'PUT' });
 }
 
-function Head(params = {}) {
-  ajax({ ...params, method: 'HEAD' });
-}
 
 const AjaxModule = {
   Get,
   Post,
   Put,
-  Head,
 };
 
 export default AjaxModule;
