acb22a660eee110140bad538493dec47aa60d9e3
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-10-13 18:57:08 +0300

    singleton bus instead of window.bus

--- src/modules/bus.js
+++ src/modules/bus.js
@@ -1,27 +1,28 @@
-(() => {
-  class Bus {
-    constructor() {
-      this.listeners = {};
-    }
+class Bus {
+  constructor() {
+    this.listeners = {};
+  }
 
-    listen(event, listener) {
-      (this.listeners[event] || (this.listeners[event] = [])).push(listener);
-    }
+  listen(event, listener) {
+    (this.listeners[event] || (this.listeners[event] = [])).push(listener);
+  }
 
-    ignore(event, listener) {
-      if (!this.listeners[event]) return;
-      if (listener) {
-        this.listeners[event] = this.listeners[event].filter(l => l !== listener);
-      } else {
-        this.listeners[event] = [];
-      }
-    }
-
-    emit(event, data = []) {
-      if (!this.listeners[event]) return;
-      this.listeners[event].forEach(l => l(...data));
+  ignore(event, listener) {
+    if (!this.listeners[event]) return;
+    if (listener) {
+      this.listeners[event] = this.listeners[event].filter(l => l !== listener);
+    } else {
+      this.listeners[event] = [];
     }
   }
 
-  window.bus = new Bus();
-})();
+  emit(event, data = []) {
+    if (!this.listeners[event]) return;
+    this.listeners[event].forEach(l => l(...data));
+  }
+}
+
+
+const bus = new Bus();
+
+export default bus;
