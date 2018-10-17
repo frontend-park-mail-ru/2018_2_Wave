a2d186434a56dd42b8fe9c89d974b2824e78c25b
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-10-17 10:59:05 +0300

    menu template updated, menu class created

---
+++ src/views/menu/menu.js
@@ -0,0 +1,19 @@
+import BaseView from '../baseview';
+
+const template = require('./menu.pug');
+
+const buttons = {
+  play: 'Play',
+  leaderboard: 'Leaderboard',
+  settings: 'Settings',
+};
+
+export default class MenuView extends BaseView {
+  constructor(parent) {
+    super(template, parent);
+  }
+
+  render() {
+    super.render(buttons);
+  }
+}
