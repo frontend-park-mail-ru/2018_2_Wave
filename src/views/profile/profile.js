d2bbff2caaeade7bcdc732802099340f185c6f0b
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-10-19 20:57:58 +0300

    ProfileView class created, seems working

---
+++ src/views/profile/profile.js
@@ -0,0 +1,26 @@
+import ajax from '../../modules/ajax';
+import BaseView from '../baseview';
+import './profile.css';
+
+const template = require('./profile.pug');
+
+
+export default class ProfileView extends BaseView {
+  constructor(parent) {
+    super(template, parent);
+  }
+
+  async update() {
+    try {
+      const user = await ajax.GET({ path: '/user' });
+      super.render({ user });
+      const logout = document.getElementById('logoutbutton');
+      logout.addEventListener('click', () => {
+        // TODO: to userService
+        ajax.POST({ path: '/user/logout' });
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
