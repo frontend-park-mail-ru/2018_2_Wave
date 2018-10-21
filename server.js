6762a7f86d449c9d6ac9d95a1fff1512c5ca3702
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-10-21 23:07:26 +0300

    global redirect to app; waste removed

--- server.js
+++ server.js
@@ -1,21 +1,17 @@
 const express = require('express');
 const favicon = require('serve-favicon');
-const cookie = require('cookie-parser');
 const morgan = require('morgan');
 const path = require('path');
-const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true,
-}));
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(favicon(path.join(__dirname, '/public/img/favicon.png')));
-app.use(cookie());
 
+app.all('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
 
 const port = process.env.PORT || 3000;
 
