893f8977c0efb9d5bfbeeb63e018e11dfefbea80
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-11-06 23:37:13 +0300

    UserBlock works with UserService

--- src/views/userblock/userblock.js
+++ src/views/userblock/userblock.js
@@ -9,40 +9,31 @@ const template = require('./userblock.pug');
 export default class UserblockView extends BaseView {
   constructor(parent) {
     super(template, parent);
-    bus.listen('userUpdated', this.update.bind(this));
+    // bus.listen('userUpdated', this.update.bind(this));
   }
 
   update() {
-    console.log('ama here');
     super.show();
     this.render();
   }
 
   async render() {
+    console.log('Rendering UserBlock', userService.isLoggedIn());
+
     if (!userService.isLoggedIn()) {
-      const user = { authorized: false };
       document.getElementById('username').innerHTML = '';
-      super.render({ user });
+      super.render({ authorized: false });
       return;
     }
 
     const user = userService.getUser();
-    console.log('User received!');
-    console.log(user);
 
-
-    // user.authorized = true;
-    // user.avatarSource = profile.avatarSource;
-    // user.name = profile.username;
-    // // TODO: FIXME: remove id
-    // document.getElementById('username').innerHTML = user.name;
-    // super.render({ user });
-    // // TODO: FIXME: remove id
-    // const profileButton = document.getElementById('userblockAvatar');
-    // profileButton.addEventListener('click', () => bus.emit('link', '/profile'));
-
-
-
-    // console.error(err);
+    const authorized = true;
+    // TODO: FIXME: remove id
+    document.getElementById('username').innerHTML = user.username;
+    super.render({ user, authorized });
+    // TODO: FIXME: remove id
+    const profileButton = document.getElementById('userblockAvatar');
+    profileButton.addEventListener('click', () => bus.emit('link', '/profile'));
   }
 }
