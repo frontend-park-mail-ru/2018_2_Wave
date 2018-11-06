03f670344ce361b9ebe58829724893182757df50
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-11-06 21:50:38 +0300

    testing UserService...

--- src/views/userblock/userblock.js
+++ src/views/userblock/userblock.js
@@ -1,4 +1,4 @@
-import { getProfile } from '../../modules/network';
+import userService from '../../modules/userservice';
 import bus from '../../modules/bus';
 import BaseView from '../baseview';
 import './userblock.css';
@@ -9,34 +9,40 @@ const template = require('./userblock.pug');
 export default class UserblockView extends BaseView {
   constructor(parent) {
     super(template, parent);
-    bus.listen('userUpdate', this.update.bind(this));
+    bus.listen('userUpdated', this.update.bind(this));
   }
 
   update() {
+    console.log('ama here');
     super.show();
     this.render();
   }
 
   async render() {
-    const user = {};
-    // TODO: get it from UserService
-    const { err, profile } = await getProfile();
-    if (!err) {
-      user.authorized = true;
-      user.avatarSource = profile.avatarSource;
-      user.name = profile.username;
-      // TODO: FIXME: remove id
-      document.getElementById('username').innerHTML = user.name;
-      super.render({ user });
-      // TODO: FIXME: remove id
-      const profileButton = document.getElementById('userblockAvatar');
-      profileButton.addEventListener('click', () => bus.emit('link', '/profile'));
-    } else {
-      user.authorized = false;
+    if (!userService.isLoggedIn()) {
+      const user = { authorized: false };
       document.getElementById('username').innerHTML = '';
       super.render({ user });
-
-      console.error(err);
+      return;
     }
+
+    const user = userService.getUser();
+    console.log('User received!');
+    console.log(user);
+
+
+    // user.authorized = true;
+    // user.avatarSource = profile.avatarSource;
+    // user.name = profile.username;
+    // // TODO: FIXME: remove id
+    // document.getElementById('username').innerHTML = user.name;
+    // super.render({ user });
+    // // TODO: FIXME: remove id
+    // const profileButton = document.getElementById('userblockAvatar');
+    // profileButton.addEventListener('click', () => bus.emit('link', '/profile'));
+
+
+
+    // console.error(err);
   }
 }
