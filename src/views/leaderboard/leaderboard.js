48e712db464b5597b89dc0406e2b32cc82ed762d
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-11-06 03:26:24 +0300

    leaderboard ready for network...

--- src/views/leaderboard/leaderboard.js
+++ src/views/leaderboard/leaderboard.js
@@ -1,4 +1,4 @@
-import ajax from '../../modules/ajax';
+import { getLeaders } from '../../modules/network';
 import BaseView from '../baseview';
 import './leaderboard.css';
 
@@ -15,24 +15,22 @@ export default class LeaderboardView extends BaseView {
     this.render(page, count);
   }
 
-  async render(page, count) {
-    try {
-      const data = await ajax.GET({
-        path: `/users/${page}/${count}`,
-      });
-      console.log(data);
-      super.render({
-        users: data.users,
-        pagesCount: (data.total / count),
-      });
-      const pagination = document.querySelector('.pagination');
-      // pagination.addEventListener('click', (event) => {
-      //   const link = event.target;
-      //   getUsers(count * (link.getAttribute('page') - 1), count);
-      // });
-    } catch (error) {
+  // TODO: FIXME: check order!!!
+  async render(count, page) {
+    const { err, leaders } = getLeaders(count, page);
+    if (err) {
       // TODO: show error
-      console.error(error);
+      console.error(err);
+      return;
     }
+    super.render({
+      users: leaders,
+      // pagesCount: (data.total / count),
+    });
+    // const pagination = document.querySelector('.pagination');
+    // pagination.addEventListener('click', (event) => {
+    //   const link = event.target;
+    // getUsers(count * (link.getAttribute('page') - 1), count);
+    // });
   }
 }
