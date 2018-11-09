ffb4c16537cbd1b9b0e4139787f1f22952af2dde
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-11-09 05:43:21 +0300

    env set in app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 import Router from './modules/router';
-import bus from './modules/bus';
 
 import UserblockView from './views/userblock/userblock';
 import MenuView from './views/menu/menu';
@@ -13,13 +12,10 @@ import ProfileEditView from './views/editprofile/editprofile';
 import './css/style.css';
 
 
-// TODO: FIXME: remove id
-const userblock = document.getElementById('userblock');
-const root = document.getElementById('root');
+const [root] = document.getElementsByClassName('root');
 
-const router = new Router(root);
-
-router
+new Router(root)
+  .setEnviroment(UserblockView)
   .register('/', MenuView)
   .register('/profile', ProfileView)
   .register('/login', LoginView)
@@ -28,6 +24,3 @@ router
   .register('/settings', SettingsView)
   .register('/leaderboard', LeaderboardView)
   .start();
-
-const userblockView = new UserblockView(userblock);
-bus.listen('userUpdated', () => { userblockView.update(); });
