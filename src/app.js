62c6bd0f7d06f4393805ec5c08f1e1e871f9eae2
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-11-19 00:32:13 +0300

    test app.js

--- src/app.js
+++ src/app.js
@@ -1,24 +1,29 @@
-import Router from './modules/router';
+// import Router from './modules/router';
 
-import UserblockView from './views/userblock/userblock';
-import MenuView from './views/menu/menu';
-import LoginView from './views/login/login';
-import RegisterView from './views/register/register';
-import LeaderboardView from './views/leaderboard/leaderboard';
-import ProfileView from './views/profile/profile';
-import SettingsView from './views/settings/settings';
-import ProfileEditView from './views/editprofile/editprofile';
+// import UserblockView from './views/userblock/userblock';
+// import MenuView from './views/menu/menu';
+// import LoginView from './views/login/login';
+// import RegisterView from './views/register/register';
+// import LeaderboardView from './views/leaderboard/leaderboard';
+// import ProfileView from './views/profile/profile';
+// import SettingsView from './views/settings/settings';
+// import ProfileEditView from './views/editprofile/editprofile';
 
-import './css/style.css';
+// import './css/style.css';
 
 
-new Router(document.body)
-  .setEnviroment(UserblockView)
-  .register('/', MenuView)
-  .register('/profile', ProfileView)
-  .register('/login', LoginView)
-  .register('/register', RegisterView)
-  .register('/profile/edit', ProfileEditView)
-  .register('/settings', SettingsView)
-  .register('/leaderboard', LeaderboardView)
-  .start();
+// new Router(document.body)
+//   .setEnviroment(UserblockView)
+//   .register('/', MenuView)
+//   .register('/profile', ProfileView)
+//   .register('/login', LoginView)
+//   .register('/register', RegisterView)
+//   .register('/profile/edit', ProfileEditView)
+//   .register('/settings', SettingsView)
+//   .register('/leaderboard', LeaderboardView)
+//   .start();
+
+import Terminal from './applications/terminal/terminal_app';
+
+const terminal = new Terminal(document.body);
+terminal.start();
