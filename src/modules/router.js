3e16883979b18179afaa871c87af6b264c2218ca
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-11-21 01:07:37 +0300

    router works with Applications

--- src/modules/router.js
+++ src/modules/router.js
@@ -1,71 +1,112 @@
 import bus from './bus';
 
 
+/*                utils                */
+
+/**  proceeds path and urlencoded params
+ *   to app name, view name and params array  */
+function processPath(fullPath) {
+  const result = {};
+  let [path, params] = fullPath.split('?');
+
+  path = path.slice(1);
+  if (fullPath.slice(-1) === '/') {
+    console.log('removing last');
+    path = path.slice(0, -1);
+  }
+
+  if (params) {
+    const paramPairs = params.split('&', 5);
+    params = {};
+    paramPairs.forEach((item) => {
+      const [key, val] = item.split('=');
+      params[key] = val;
+    });
+    result.params = params;
+  }
+
+  const separator = path.indexOf('/');
+  if (separator !== -1) {
+    const app = path.slice(0, separator);
+    const view = path.slice(separator + 1);
+    result.app = app;
+    result.view = view;
+  } else {
+    result.app = path;
+  }
+
+  return result;
+}
+
+
 export default class Router {
   constructor(root) {
     this.routes = {};
     this.root = root;
 
-    this.enviromentElem = document.createElement('div');
-    this.enviromentElem.classList.add('enviroment');
-    this.root.appendChild(this.enviromentElem);
+    // this.enviromentElem = document.createElement('div');
+    // this.enviromentElem.classList.add('enviroment');
+    // this.root.appendChild(this.enviromentElem);
 
-    this.contentElem = document.createElement('div');
-    this.contentElem.classList.add('content');
-    this.root.appendChild(this.contentElem);
+    // this.contentElem = document.createElement('div');
+    // this.contentElem.classList.add('content');
+    // this.root.appendChild(this.contentElem);
   }
 
 
-  setEnviroment(View) {
-    this.enviroment = new View(this.enviromentElem);
-    return this;
-  }
+  // setMainApp(App) {
+  //   this.enviroment = new View(this.enviromentElem);
+  //   return this;
+  // }
 
-  register(path, View) {
-    this.routes[path] = new View(this.root);
+  registerApp(url, App) {
+    const app = new App(url, this.root);
+    this.routes[url] = app;
     return this;
   }
 
 
   open(path) {
-    const openingView = this.routes[path.split('?', 1)];
-    const params = {};
-    window.location.search
-      .substr(1)
-      .split('&', 5)
-      .forEach((item) => {
-        const [key, val] = item.split('=');
-        params[key] = val;
+    console.log(path);
+
+    let { app, view, params } = processPath(path);
+    console.log({ app, view, params });
+
+    if (!app) app = this.routes['/'];
+    else if (!this.routes.hasOwnProperty(app)) {
+      app = this.routes['/'];
+      view = null;
+      params = null;
+    } else app = this.routes[app];
+
+    if (view && app.views.hasOwnProperty(view)) {
+      app.changeView(view, params);
+    }
+
+    if (!app.active) {
+      Object.values(this.routes).forEach((knownApp) => {
+        // TODO: make array of active apps
+        // TODO: check for main app, never pause it
+        // TODO: stop old apps
+        if (knownApp.active) knownApp.pause();
       });
 
-    if (!openingView) {
-      this.open('/');
-      return;
+      if (!app.started) app.start();
+      else app.resume();
     }
+
 
     if (window.location.pathname !== path) {
       window.history.pushState(null, '', path);
     }
-
-    if (!openingView.active) {
-      Object.values(this.routes).forEach((view) => {
-        if (view.active) view.hide();
-      });
-
-      if (Object.prototype.hasOwnProperty.call(this, 'enviroment')) {
-        if (openingView.single) this.enviroment.hide();
-        else                    this.enviroment.show();
-      }
-
-      openingView.show(params);
-    }
-
-    this.routes[path] = openingView;
   }
 
 
   start() {
-    this.open(window.location.pathname);
+    this.open(
+      window.location.pathname
+      + window.location.search,
+    );
 
     document.addEventListener('click', (event) => {
       if (!(event.target instanceof HTMLAnchorElement)
@@ -75,12 +116,15 @@ export default class Router {
       event.preventDefault();
 
       const link = event.target;
-      this.open(link.pathname);
+      this.open(link.pathname + link.params);
     });
 
     window.addEventListener(
       'popstate',
-      () => this.open(window.location.pathname),
+      () => this.open(
+        window.location.pathname
+        + window.location.search,
+      ),
     );
 
     bus.listen('link', this.open.bind(this));
