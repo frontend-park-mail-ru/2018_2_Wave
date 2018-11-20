4235bd473fdf8393c43d62d354b1b7a39243695c
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-11-21 01:26:27 +0300

    try to start apps using router

--- src/app.js
+++ src/app.js
@@ -1,29 +1,14 @@
-// import Router from './modules/router';
+import Router from './modules/router';
 
-// import UserblockView from './views/userblock/userblock';
-// import MenuView from './views/menu/menu';
-// import LoginView from './views/login/login';
-// import RegisterView from './views/register/register';
-// import LeaderboardView from './views/leaderboard/leaderboard';
-// import ProfileView from './views/profile/profile';
-// import SettingsView from './views/settings/settings';
-// import ProfileEditView from './views/editprofile/editprofile';
+import Terminal from './applications/terminal/terminal_app';
 
 // import './css/style.css';
 
 
-// new Router(document.body)
-//   .setEnviroment(UserblockView)
-//   .register('/', MenuView)
-//   .register('/profile', ProfileView)
-//   .register('/login', LoginView)
-//   .register('/register', RegisterView)
-//   .register('/profile/edit', ProfileEditView)
-//   .register('/settings', SettingsView)
-//   .register('/leaderboard', LeaderboardView)
-//   .start();
+new Router(document.body)
+  .registerApp('/', Terminal)
+  .start();
 
-import Terminal from './applications/terminal/terminal_app';
 
-const terminal = new Terminal(document.body);
-terminal.start();
+// const terminal = new Terminal(document.body);
+// terminal.start();
