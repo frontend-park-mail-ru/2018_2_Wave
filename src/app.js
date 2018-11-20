1a6317ef46189c088db5d59f281520a1cd267572
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-11-21 02:22:34 +0300

    styles moved to menu application

--- src/app.js
+++ src/app.js
@@ -1,14 +1,7 @@
 import Router from './modules/router';
-
 import Terminal from './applications/terminal/terminal_app';
-
-// import './css/style.css';
 
 
 new Router(document.body)
   .registerApp('/', Terminal)
   .start();
-
-
-// const terminal = new Terminal(document.body);
-// terminal.start();
