3be430bb03317731237eabf0dff402d107ce50a1
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-11-21 18:44:03 +0300

    beta-router: many-many-many changes

--- src/modules/router.js
+++ src/modules/router.js
@@ -3,35 +3,24 @@ import bus from './bus';
 
 /*                utils                */
 
-/**  proceeds path and urlencoded params
- *   to app name, view name and params array  */
-function processPath(fullPath, params) {
-  const result = {};
+/**  proceeds urlencoded params to array  */
+function splitParams(string) {
+  const params = {};
+  string
+    .split('&', 5)
+    .forEach((item) => {
+      const [key, val] = item.split('=');
+      params[key] = val;
+    });
+  return params;
+}
 
-  let path = fullPath.slice(1);
-  if (path.slice(-1) === '/') {
-    path = path.slice(0, -1);
-  }
-
-  if (params) {
-    result.params = {};
-    params
-      .split('&', 5)
-      .forEach((item) => {
-        const [key, val] = item.split('=');
-        result.params[key] = val;
-      });
-  }
-
-  const separator = path.indexOf('/');
-  if (separator !== -1) {
-    result.app = path.slice(0, separator);
-    result.view = path.slice(separator + 1);
-  } else {
-    result.app = path;
-  }
-
-  return result;
+/**  removes slashes from path string  */
+function clearPath(string) {
+  if (string === '/') return string;
+  const path = string.slice(1);
+  if (path.slice(-1) === '/') return path.slice(0, -1);
+  return path;
 }
 
 
@@ -39,65 +28,6 @@ export default class Router {
   constructor(root) {
     this.routes = {};
     this.root = root;
-  }
-
-  // TODO: write this method
-  // TODO: grant some previleges to main app
-  // TODO: get wrapper-div from main app
-
-  // setMainApp(App) {
-  //   this.enviroment = new View(this.enviromentElem);
-  //   return this;
-  // }
-
-  registerApp(url, App) {
-    const app = new App(url, this.root);
-    // TODO: create app object only when app opens
-    this.routes[url] = app;
-    return this;
-  }
-
-
-  open(path) {
-    const { app: appName, view, params } = processPath(path);
-    console.log({ appName, view, params });
-
-
-    let app;
-    if (!appName) app = this.routes['/'];
-    else if (!this.routes.hasOwnProperty(appName)) {
-      if (this.routes['/'].hasOwnProperty(app)) {
-
-      }
-      this.open('/');
-      return;
-    } else app = this.routes[appName];
-
-    if (view) {
-      if (app.views.hasOwnProperty(view)) {
-        app.changeView(view, params);
-      } else {
-        this.open(appName);
-        return;
-      }
-    }
-
-    if (!app.active) {
-      Object.values(this.routes).forEach((knownApp) => {
-        // TODO: make array of active apps
-        // TODO: check for main app, never pause it
-        // TODO: stop old apps
-        if (knownApp.active) knownApp.pause();
-      });
-
-      if (!app.started) app.start();
-      else app.resume();
-    }
-
-
-    if (window.location.pathname !== path) {
-      window.history.pushState(null, '', path);
-    }
   }
 
 
@@ -126,5 +56,60 @@ export default class Router {
     });
 
     bus.listen('link', this.open.bind(this));
+  }
+
+
+  open(fullPath, paramString) {
+    console.log(fullPath, paramString);
+
+
+    const path = clearPath(fullPath);
+    const params = paramString
+      ? splitParams(paramString)
+      : null;
+
+    let app;
+    if (this.routes.hasOwnProperty(path)) {
+      app = this.routes[path];
+      if (app === this.mainApp) app.changeView('main', params);
+    } else if (this.mainApp.views.hasOwnProperty(path)) {
+      app = this.mainApp;
+      app.changeView(path, params);
+    } else {
+      this.open('/');
+    }
+
+    if (!app.active) {
+      Object.values(this.routes).forEach((knownApp) => {
+        // TODO: make array of active apps
+        // TODO: check for main app, never pause it
+        // TODO: stop old apps
+        if (knownApp.active) knownApp.pause();
+      });
+      if (!app.started) app.start();
+      else app.resume();
+    }
+
+    if (window.location.pathname !== fullPath) {
+      window.history.pushState(null, '', fullPath);
+    }
+  }
+
+
+  // TODO: write this method
+  // TODO: grant some previleges to main app
+  // TODO: get wrapper-div from main app
+
+  // setMainApp(App) {
+  //   this.enviroment = new View(this.enviromentElem);
+  //   return this;
+  // }
+
+  registerApp(url, App) {
+    const app = new App(url, this.root);
+    if (url === '/') this.mainApp = app;
+    // TODO: create app object only when app opens
+    this.routes[url] = app;
+    return this;
   }
 }
