84ffcc952edbb7c6bda990a9d42f4c361914c4c9
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-11-23 23:05:20 +0300

    snake registered as app

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import Router from './modules/router';
 import Terminal from './applications/terminal/terminal_app';
 import Menu from './applications/menu/menu_app';
+import Snake from './applications/snake/game_app';
 
 new Router(document.body)
   .registerApp('/', Menu)
   .registerApp('terminal', Terminal)
+  .registerApp('snake', Snake)
   .start();
