0b53b57cf592788cbf14a7d9d7d5ea359837e138
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-11-23 23:03:05 +0300

    snake application

--- src/applications/snake/game_app.js
+++ src/applications/snake/game_app.js
@@ -1,7 +1,11 @@
+import GAME_MODES from './game/core/modes';
+
+
 import BaseApp from '../base_app';
 
 import GameEnv from './views/env';
 import GameView from './views/game_field';
+import Game from './game/game';
 
 
 export default class SnakeApp extends BaseApp {
@@ -10,22 +14,68 @@ export default class SnakeApp extends BaseApp {
     super(appUrl, env.getContainer(), GameView);
 
     this.env = env;
-    this.content = this.env.getContainer();
+    this.gameContainer = this.views.main.getCanvas();
   }
 
 
   start() {
     this.env.show();
     super.start();
+
+    this.initGame();
   }
 
   pause() {
     this.env.hide();
     super.pause();
+
+
+    // TODO: FIXME: achtung!!!
+    this.game.destroy();
   }
 
   resume() {
     this.env.show();
     super.resume();
+  }
+
+
+  stop() {
+    this.game.destroy();
+  }
+
+
+  initGame() {
+    // TODO think about chosing game mode
+    /*
+    if (pathname === '/game/online-mode') {
+      mode = GAME_MODES.ONLINE;
+    } else {
+      mode = GAME_MODES.OFFLINE;
+    }
+    */
+
+
+    const gameInitData = {
+      snakeText: 'qwertyuiopqe',
+      DOMRect: {
+        x: 10,
+        y: 10,
+        width: 18,
+        height: 18,
+      },
+      // windowWidth: window.innerWidth,
+      // windowHeight: window.innerHeight,
+      windowWidth: 800,
+      windowHeight: 800,
+    };
+
+    const mode = navigator.onLine
+      ? GAME_MODES.ONLINE
+      : GAME_MODES.OFFLINE;
+    this.game = new Game(mode, this.gameContainer, gameInitData);
+
+    // TODO: FIXME: call this in view after button press!
+    this.game.start();
   }
 }
