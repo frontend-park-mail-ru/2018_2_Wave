4723deadd258dd0c5e16d12fa960ce0905a18d22
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-11-23 23:02:50 +0300

    some logic moved to game_app from game_field

--- src/applications/snake/views/game_field.js
+++ src/applications/snake/views/game_field.js
@@ -1,13 +1,11 @@
 import Element from '../../element';
-import Game from '../game/core/game';
-import GAME_MODES from '../game/core/modes';
 
 import template from '../templates/game.pug';
+
 
 export default class GameView extends Element {
   constructor(parent) {
     super(template, parent);
-    this.game = null;
 
     // on the future
     /*
@@ -19,51 +17,9 @@ export default class GameView extends Element {
     */
   }
 
-
-  destroy() {
-    this.game.destroy();
-    return this;
-  }
-
-  create() {
-    // super.create();
-
-    this.doGame();
-    return this;
-  }
-
-  doGame() {
-    let mode = '';
-    // TODO think about chosing game mode
-    /*
-    if (pathname === '/game/online-mode') {
-      mode = GAME_MODES.ONLINE;
-    } else {
-      mode = GAME_MODES.OFFLINE;
-    }
-    */
-
-    if (navigator.onLine) {
-      mode = GAME_MODES.ONLINE;
-    } else {
-      mode = GAME_MODES.OFFLINE;
-    }
-
-    const gameInitData = {
-      snakeText: 'qwertyuiopqe',
-      DOMRect: {
-        x: 10,
-        y: 10,
-        width: 18,
-        height: 18,
-      },
-      // windowWidth: window.innerWidth,
-      // windowHeight: window.innerHeight,
-      windowWidth: 800,
-      windowHeight: 800,
-    };
-
-    this.game = new Game(mode, this.wrapper, gameInitData);
-    this.game.start();
+  getCanvas() {
+    if (!this.rendered) this.render();
+    [this.canvas] = this.wrapper.getElementsByClassName('canvas');
+    return this.canvas;
   }
 }
