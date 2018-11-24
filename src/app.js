17a701856018674a53efd62035c34cff4650fad3
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-11-24 12:23:50 +0300

    menu app hidden

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
 import Router from './modules/router';
 import Terminal from './applications/terminal/terminal_app';
-import Menu from './applications/menu/menu_app';
+// import Menu from './applications/menu/menu_app';
 import Snake from './applications/snake/game_app';
 
-import css from './style.css';
-
 new Router(document.body)
-  .registerApp('/', Menu)
-  .registerApp('terminal', Terminal)
+  // .registerApp('/', Menu)
+  .registerApp('/', Terminal)
   .registerApp('snake', Snake)
   .start();
