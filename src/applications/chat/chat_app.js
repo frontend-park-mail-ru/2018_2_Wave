f9438523db1a0c1f58a6005cd4c06f2e7ef03dfa
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-11-25 14:11:25 +0300

    ChatApp start()

--- src/applications/chat/chat_app.js
+++ src/applications/chat/chat_app.js
@@ -1,10 +1,15 @@
 import BaseApp from '../base_app';
-
 import ChatView from './views/chat';
 
 
 export default class ChatApp extends BaseApp {
   constructor(url, parent) {
     super(url, parent, ChatView);
+    this.chat = this.views.main;
+  }
+
+  start() {
+    super.start();
+    this.chat.updateChats();
   }
 }
