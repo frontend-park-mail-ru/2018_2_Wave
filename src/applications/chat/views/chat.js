c2e6823ab659aad3de92eb4e5ff8eadcd6c7bb96
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-11-25 14:12:11 +0300

    updateChats()

--- src/applications/chat/views/chat.js
+++ src/applications/chat/views/chat.js
@@ -1,9 +1,38 @@
 import Element from '../../element';
 
-import template from '../templates/chat.pug';
+import chatTemplate from '../templates/chat.pug';
+import roomTemplate from '../templates/room.pug';
+
+import '../styles/chat.css';
+import '../styles/room.css';
 
 export default class ChatView extends Element {
   constructor(parent) {
-    super(template, parent);
+    super(chatTemplate, parent);
+    super.render();
+    [this.roomList] = this.wrapper.getElementsByClassName('rooms');
+  }
+
+
+  updateChats() {
+    // get chat list
+    const rooms = [
+      {
+        avatar: '/img/avatars/default',
+        id: 'vasya',
+        message: 'hello',
+      },
+      {
+        avatar: '/img/avatars/default',
+        id: 'vasya',
+        message: 'hello',
+      },
+    ];
+
+    this.roomList.innerHTML = '';
+
+    rooms.forEach((room) => {
+      this.roomList.innerHTML += roomTemplate({ room });
+    });
   }
 }
