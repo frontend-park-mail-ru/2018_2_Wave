da261e95e8399ea9c6b4b543a483d42dd61855cc
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-11-25 14:10:19 +0300

    styles added to main app

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import Menu from './applications/menu/menu_app';
 import Snake from './applications/snake/game_app';
 import Chat from './applications/chat/chat_app';
 
+import './style.css';
 
 new Router(document.body)
   .registerApp('/', Menu)
