867d87a17eec9824cafd69f70dc62bf197db590e
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-11-28 01:03:23 +0300

    service worker in src

---
+++ src/sw.js
@@ -0,0 +1,12 @@
+this.addEventListener('install', () => {
+    console.log('Установлен');
+  });
+   // this.addEventListener('activate', () => {
+  //   console.log('Активирован');
+  // });
+   // this.addEventListener('fetch', () => {
+  //   console.log('Происходит запрос на сервер');
+  // });
+   // this.addEventListener('install', function(event) {
+    
+  // });
