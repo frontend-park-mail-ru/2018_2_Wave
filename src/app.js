a8a173741aa9061ec844cef0a211c8f67baaed22
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-11-29 00:08:45 +0300

    trying to make favicon cache

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ import Terminal from './applications/terminal/terminal_app';
 import Menu from './applications/menu/menu_app';
 import Snake from './applications/snake/game_app';
 
-
 new Router(document.body)
   .registerApp('/', Menu)
   .registerApp('terminal', Terminal)
