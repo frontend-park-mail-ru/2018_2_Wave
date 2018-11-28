40199cecfb7d7028c5edf6622cc61bdba45db768
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-11-28 11:37:46 +0300

    sw register improved

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import Terminal from './applications/terminal/terminal_app';
 import Menu from './applications/menu/menu_app';
 import Snake from './applications/snake/game_app';
 
+
 new Router(document.body)
   .registerApp('/', Menu)
   .registerApp('terminal', Terminal)
@@ -11,24 +12,7 @@ new Router(document.body)
 
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./sw.js')
-    .then((registration) => {
-      // Registration was successful
-      console.log('SW registration OK:', registration);
-    })
-    .catch((err) => {
-      // registration failed :(
-      console.log({ err });
-    });
+  navigator.serviceWorker
+    .register('./sw.js')
+    .catch(err => console.log({ err }));
 }
-
-
-// if ('serviceWorker' in navigator) {
-//   navigator.serviceWorker.register('/sw-test/sw.js', { scope: '/sw-test/' }).then(function(reg) {
-//     // регистрация сработала
-//     console.log('Registration succeeded. Scope is ' + reg.scope);
-//   }).catch(function(error) {
-//     // регистрация прошла неудачно
-//     console.log('Registration failed with ' + error);
-//   });
-// };
