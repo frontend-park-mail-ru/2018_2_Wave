9f3548ca4fab7cda8d3817de5e77e68b7fde6bf2
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-11-28 17:56:36 +0300

    service worker

--- src/sw.js
+++ src/sw.js
@@ -1,12 +1,34 @@
-this.addEventListener('install', () => {
-    console.log('Установлен');
-  });
-   // this.addEventListener('activate', () => {
-  //   console.log('Активирован');
-  // });
-   // this.addEventListener('fetch', () => {
-  //   console.log('Происходит запрос на сервер');
-  // });
-   // this.addEventListener('install', function(event) {
-    
-  // });
+/* eslint no-restricted-globals: 1 */
+
+const KEY = 'vawe!';
+const { assets } = global.serviceWorkerOption;
+
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(KEY)
+      .then((cache) => {
+        console.log('cache open');
+        return cache.addAll(assets);
+      })
+      .catch(err => console.log({ err })),
+  );
+});
+
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((cachedResponse) => {
+        if (cachedResponse) return cachedResponse;
+        return fetch(event.request)
+          .then(response => caches.open(KEY)
+            .then((cache) => {
+              cache.put(event.request, response.clone());
+              return response;
+            }))
+          .catch(() => cachedResponse);
+      })
+      .catch(error => console.log(error)),
+  );
+});
