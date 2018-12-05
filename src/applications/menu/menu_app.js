469141b1107ffd46475581b011b74385f511583b
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-12-05 03:09:46 +0300

    menu_app commented for tests

--- src/applications/menu/menu_app.js
+++ src/applications/menu/menu_app.js
@@ -2,33 +2,32 @@ import BaseApp from '../base_app';
 
 import Enviroment from './views/env';
 import MenuView from './views/menu';
-import LoginView from './views/login';
-import RegisterView from './views/register';
-import LeaderboardView from './views/leaderboard';
-import ProfileView from './views/profile';
-import SettingsView from './views/settings';
-import ProfileEditView from './views/editprofile';
+// import LoginView from './views/login';
+// import RegisterView from './views/register';
+// import LeaderboardView from './views/leaderboard';
+// import ProfileView from './views/profile';
+// import SettingsView from './views/settings';
+// import ProfileEditView from './views/editprofile';
 
-import './styles/style.css';
 
 
 export default class MenuApp extends BaseApp {
   constructor(appUrl, parent) {
     const env = new Enviroment(parent);
-    const Views = {
-      login: LoginView,
-      register: RegisterView,
-      leaderboard: LeaderboardView,
-      profile: ProfileView,
-      settings: SettingsView,
-      'profile/edit': ProfileEditView,
-    };
+    // const Views = {
+    //   login: LoginView,
+    //   register: RegisterView,
+    //   leaderboard: LeaderboardView,
+    //   profile: ProfileView,
+    //   settings: SettingsView,
+    //   'profile/edit': ProfileEditView,
+    // };
 
-    super(appUrl, env.getContainer(), MenuView, Views);
+    // super(appUrl, env.getContainer(), MenuView, Views);
+    super(appUrl, parent, MenuView);
 
     this.env = env;
-    this.content = this.env.getContainer();
-
+    // this.content = this.env.getContainer();
 
     // this.appWrapper = ???
     // hmmm let me think what kind of shit it should be
@@ -37,20 +36,16 @@ export default class MenuApp extends BaseApp {
 
   start() {
     this.env.show();
-    super.start();
+    // super.start();
   }
 
-  // stop() {
-  //   console.error('Cannot stop main app');
+  // pause() {
+  //   this.env.hide();
+  //   super.pause();
   // }
 
-  pause() {
-    this.env.hide();
-    super.pause();
-  }
-
-  resume() {
-    this.env.show();
-    super.resume();
-  }
+  // resume() {
+  //   this.env.show();
+  //   super.resume();
+  // }
 }
