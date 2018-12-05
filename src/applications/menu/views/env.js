1d00bef6238878841e255722d3d05e7c5c7a6819
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-12-05 03:15:51 +0300

    pcss loads in menu env view

--- src/applications/menu/views/env.js
+++ src/applications/menu/views/env.js
@@ -1,16 +1,21 @@
 import Element from '../../element';
-import UserBlock from '../components/userblock';
+// import UserBlock from '../components/userblock';
 
 
 import template from '../templates/main.pug';
+
+
+import '../styles/main.pcss';
+import '../styles/header.pcss';
+import '../styles/rename.pcss';
 
 
 export default class Enviroment extends Element {
   constructor(parent) {
     super(template, parent);
     super.render();
-    const [userblockPlace] = this.wrapper.getElementsByClassName('header');
-    this.userblock = new UserBlock(userblockPlace);
+    // const [userblockPlace] = this.wrapper.getElementsByClassName('header');
+    // this.userblock = new UserBlock(userblockPlace);
   }
 
   getContainer() {
