9043aa20d20b44b3ca459b506b37add89d86bffe
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-12-07 20:27:15 +0300

    testing: Terminal and Snake apps mutes

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 import Router from './modules/router';
-import Terminal from './applications/terminal/terminal_app';
+// import Terminal from './applications/terminal/terminal_app';
 import Menu from './applications/menu/menu_app';
-import Snake from './applications/snake/game_app';
+// import Snake from './applications/snake/game_app';
 
 import '../static/favicon.ico';
 import './style.pcss';
 
 new Router(document.body)
   .registerApp('/', Menu)
-  .registerApp('terminal', Terminal)
-  .registerApp('snake', Snake)
+  // .registerApp('terminal', Terminal)
+  // .registerApp('snake', Snake)
   .start();
 
 
