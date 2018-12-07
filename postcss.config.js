6f6f808af1ab578a3ea123711b1836e9502ab21d
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-12-07 18:54:43 +0300

    postcss-imports

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   plugins: [
+    require('postcss-import'),
     require('precss'),
     require('postcss-cssnext'),
     require('postcss-google-font'),
-  ]
+    // require('postcss-font-magician')({
+    //   hosted: ['./Gilroy-FREE'],
+    // }),
+  ],
 }
