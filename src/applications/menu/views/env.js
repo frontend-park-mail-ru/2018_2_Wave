62877d5101a357150cd1a3ca0f80b69f554af71c
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-12-10 14:17:35 +0300

    mainapp enviroment view done

--- src/applications/menu/views/env.js
+++ src/applications/menu/views/env.js
@@ -1,43 +1,41 @@
 import Element from '../../element';
-// import UserBlock from '../components/userblock';
-
+import UserBlock from '../components/userblock/userblock';
+import Menu from '../components/menu/menu';
 
 import template from '../templates/main.pug';
-
 import '../styles/main.pcss';
-import '../styles/tile.pcss';
-import '../styles/caroosel.pcss';
-import '../styles/content.pcss';
-import '../styles/header.pcss';
-import '../styles/menu.pcss';
-import '../styles/rename.pcss';
-import '../styles/tile-panel.pcss';
 
 import '../../../../static/fonts/Gilroy-ExtraBold.otf';
 import '../../../../static/fonts/Gilroy-Light.otf';
 
-import '../../../../static/img/triss.jpg';
-import '../../../../static/img/terminal.jpg';
 
 export default class Enviroment extends Element {
   constructor(parent) {
     super(template, parent);
     super.render();
-    // const [userblockPlace] = this.wrapper.getElementsByClassName('header');
-    // this.userblock = new UserBlock(userblockPlace);
+
+    [this.contentPlace] = this.wrapper.getElementsByClassName('content');
+    [this.title] = this.wrapper.getElementsByClassName('title__text');
+
+    const [userblockPlace] = this.wrapper.getElementsByClassName('userblock');
+    const [menuPlace] = this.wrapper.getElementsByClassName('menu');
+    this.userblock = new UserBlock(userblockPlace, userblockPlace);
+    this.menu = new Menu(menuPlace, menuPlace);
   }
 
-  getContainer() {
-    [this.content] = this.wrapper.getElementsByClassName('content');
-    return this.content;
+  setTitle(text) {
+    this.title.innerHTML = text;
+    document.title = text;
   }
 
   show() {
     super.show();
-    // this.userblock.show();
+    this.userblock.show();
+    this.menu.show();
   }
 
   async render() {
-    // this.userblock.render();
+    this.userblock.render();
+    this.menu.render();
   }
 }
