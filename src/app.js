d93ecb68fb21892d75f4e6baafeed70a21ba4a49
ksgl <ksenia.glazacheva@gmail.com>
2018-12-11 23:05:47 +0300

    gameapp works, router .registerGame works but strangely, hope someone shall help me with it

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-// import Router from './modules/router';
+import Router from './modules/router';
 // import Terminal from './applications/terminal/terminal_app';
-// import Menu from './applications/menu/menu_app';
+import Menu from './applications/menu/menu_app';
 // import Snake from './applications/snake/game_app';
 
 // import '../static/favicon.ico';
@@ -18,7 +18,16 @@
 //     .catch(err => console.log({ err }));
 // }
 
+/*
 import Frame from './applications/frame/frame_view';
 
 const frame = new Frame(document.body);
 frame.draw('https://www.innogames.com/games/all-games/');
+*/
+
+import Game from './applications/game/game_app';
+
+new Router(document.body)
+  .registerApp('/', Menu)
+  .registerGame('/magurl', Game, 'https://www.innogames.com/games/all-games/')
+  .start();
