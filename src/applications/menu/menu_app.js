3732ba960015e261c8862024755675dcef2ea7f9
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-12-13 21:25:55 +0300

    smooth menu bluring on pause

--- src/applications/menu/menu_app.js
+++ src/applications/menu/menu_app.js
@@ -4,6 +4,7 @@ import BaseApp from '../base_app';
 
 import Enviroment from './views/enviroment/env';
 import LibraryView from './views/library/library';
+import AppContainer from './views/app_container/app_container';
 // import LoginView from './views/login';
 // import RegisterView from './views/register';
 // import LeaderboardView from './views/leaderboard';
@@ -13,6 +14,9 @@ import LibraryView from './views/library/library';
 
 export default class MenuApp extends BaseApp {
   constructor(appUrl, parent) {
+    // const envWrapper = document.createElement('div');
+    // envWrapper.classList.add('envWrapper');
+    // const env = new Enviroment(parent, envWrapper);
     const env = new Enviroment(parent, parent);
     // const Views = {
     //   login: LoginView,
@@ -24,29 +28,45 @@ export default class MenuApp extends BaseApp {
     // };
 
     super(appUrl, env.contentPlace, LibraryView);
-    // super(appUrl, parent, LibraryView);
 
     this.env = env;
     this.content = this.env.content;
     this.menu = this.env.menu;
 
-    // this.appWrapper = ???
-    // hmmm let me think what kind of shit it should be
+    this.appContainer = new AppContainer(
+      this.env.appContainerPlace,
+      this.env.appContainerPlace,
+    );
   }
 
 
   start() {
+    this.appContainer.hide();
     this.env.show();
     super.start();
   }
 
   pause() {
-    this.env.hide();
-    super.pause();
+    this.active = false;
+    this.env.mainContainer.classList.add('blurred');
+    this.appContainer.show();
   }
 
   resume() {
-    this.env.show();
-    super.resume();
+    if (!this.started) {
+      this.start();
+      return;
+    }
+
+    this.active = true;
+
+    const { mainContainer } = this.env;
+
+    mainContainer.classList.remove('blurred');
+    mainContainer.classList.add('unblurred');
+    mainContainer.addEventListener('animationend', () => {
+      mainContainer.classList.remove('unblurred');
+      this.appContainer.hide();
+    }, { once: true });
   }
 }
