7f89ce74f69b4072647fbed86b7303c9e492d0f9
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-12-15 00:36:43 +0300

    menu hiding and showing animation

--- src/applications/menu/components/menu/menu.js
+++ src/applications/menu/components/menu/menu.js
@@ -33,14 +33,40 @@ const tiles = [
 export default class Menu extends Element {
   constructor(parent, wrapper) {
     super(template, parent, wrapper);
+
+
+    this.showAnimation = this.wrapper.animate({
+      transform: [
+        'translateY(250px)',
+        'translateY(0px)',
+      ],
+    }, {
+      duration: 200,
+      fill: 'forwards',
+      easing: 'cubic-bezier(.36,1.08,.55,.93)',
+    });
+    this.showAnimation.pause();
   }
 
   show() {
-    super.show();
     if (!this.rendered) {
       // render only one time, because menu is unchangeable
       this.render();
     }
+
+    this.showAnimation.onfinish = () => {
+      super.show();
+    };
+
+    this.showAnimation.play();
+  }
+
+  hide() {
+    this.showAnimation.onfinish = () => {
+      super.hide();
+    };
+
+    this.showAnimation.reverse();
   }
 
   render() {
