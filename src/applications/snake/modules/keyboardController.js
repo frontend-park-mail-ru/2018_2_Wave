f28289f8f5dc07227c6e02155a829af0767d8f1f
dlipko <lipko_dima@mail.ru>
2018-12-18 02:28:56 +0300

    eneter from terminal magic

--- src/applications/snake/modules/keyboardController.js
+++ src/applications/snake/modules/keyboardController.js
@@ -35,8 +35,8 @@ class KeyboardController {
 
   start() {
     // magic here
-    // setTimeout(() => document.addEventListener('keydown', this.acceptInput.bind(this), 1));
-    document.addEventListener('keydown', this.acceptInput);
+    setTimeout(() => document.addEventListener('keydown', this.acceptInput.bind(this), 1));
+    // document.addEventListener('keydown', this.acceptInput);
     this.swipeDetector.start();
   }
 
