f6e45550c75d232286a67aaae5116161bf0b9215
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-12-19 04:46:34 +0300

    mock for app categories

---
+++ src/applications/main/views/store/mock.js
@@ -0,0 +1,92 @@
+const storeApps = {
+  all: [
+    {
+      link: '/terminal',
+      image: '/img/terminal.jpg',
+      name: 'Купи говно',
+    },
+    {
+      link: '/terminal',
+      image: '/img/terminal.jpg',
+      name: 'Купи говно',
+    },
+    {
+      link: '/terminal',
+      image: '/img/terminal.jpg',
+      name: 'Купи говно',
+    },
+    {
+      link: '/terminal',
+      image: '/img/terminal.jpg',
+      name: 'Купи говно',
+    },
+  ],
+  new: [
+    {
+      link: '/terminal',
+      image: '/img/terminal.jpg',
+      name: 'Купи говно',
+    },
+    {
+      link: '/terminal',
+      image: '/img/terminal.jpg',
+      name: 'Купи говно',
+    },
+    {
+      link: '/terminal',
+      image: '/img/terminal.jpg',
+      name: 'Купи говно',
+    },
+    {
+      link: '/terminal',
+      image: '/img/terminal.jpg',
+      name: 'Купи говно',
+    },
+  ],
+  popular: [
+    {
+      link: '/terminal',
+      image: '/img/terminal.jpg',
+      name: 'Купи говно',
+    },
+    {
+      link: '/terminal',
+      image: '/img/terminal.jpg',
+      name: 'Купи говно',
+    },
+    {
+      link: '/terminal',
+      image: '/img/terminal.jpg',
+      name: 'Купи говно',
+    },
+    {
+      link: '/terminal',
+      image: '/img/terminal.jpg',
+      name: 'Купи говно',
+    },
+  ],
+  '2018_2': [
+    {
+      link: '/terminal',
+      image: '/img/terminal.jpg',
+      name: 'Купи говно',
+    },
+    {
+      link: '/terminal',
+      image: '/img/terminal.jpg',
+      name: 'Купи говно',
+    },
+    {
+      link: '/terminal',
+      image: '/img/terminal.jpg',
+      name: 'Купи говно',
+    },
+    {
+      link: '/terminal',
+      image: '/img/terminal.jpg',
+      name: 'Купи говно',
+    },
+  ],
+};
+
+export default storeApps;
