3054849946f66e40d322060bda7716663d0e06f9
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-12-19 03:48:22 +0300

    additional tile

--- src/applications/main/views/store/store.js
+++ src/applications/main/views/store/store.js
@@ -1,5 +1,6 @@
 import Element from '../../../element';
 import AppTile from '../../components/app_tile/app_tile';
+import List from '../../components/list/list';
 
 import template from './store.pug';
 import '../../components/app_tile/app_tile.pcss';
@@ -8,6 +9,11 @@ import './store.pcss';
 import '../../../../../static/img/terminal.jpg';
 
 const storeApps = [
+  {
+    link: '/terminal',
+    image: '/img/terminal.jpg',
+    name: 'Купи говно',
+  },
   {
     link: '/terminal',
     image: '/img/terminal.jpg',
@@ -35,6 +41,8 @@ export default class StoreView extends Element {
 
     this.parentViews = views;
     [this.panel] = this.wrapper.getElementsByClassName('store__tile-panel');
+    [this.listPlace] = this.parentViews.main.wrapper.getElementsByClassName('list');
+    this.list = new List(this.listPlace, this.listPlace);
   }
 
   render() {
@@ -46,11 +54,21 @@ export default class StoreView extends Element {
   }
 
   show() {
+    const [grid] = document.getElementsByClassName('grid-common');
+    if (!grid.classList.contains('store__grid')) {
+      grid.classList.add('store__grid');
+    }
+
     super.show();
     this.parentViews.env.menu.hide();
   }
 
   hide() {
+    const [grid] = document.getElementsByClassName('grid-common');
+    if (grid.classList.contains('store__grid')) {
+      grid.classList.remove('store__grid');
+    }
+
     super.hide();
     this.parentViews.env.menu.show();
   }
