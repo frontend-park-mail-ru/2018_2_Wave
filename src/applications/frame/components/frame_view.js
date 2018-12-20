fe1070a11abe5e2ae5424c1697a492435767d40e
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-12-20 03:18:42 +0300

    lag fixed

--- src/applications/frame/components/frame_view.js
+++ src/applications/frame/components/frame_view.js
@@ -8,9 +8,11 @@ export default class FrameView extends Element {
   constructor(parent, wrapper, source) {
     super(template, parent, wrapper);
     this.source = source;
+    this.noRender = true;
   }
 
   render() {
+    console.log('here');
     // [this.frame] = this.wrapper.getElementsByClassName('frame');
     super.render({ source: this.source });
   }
