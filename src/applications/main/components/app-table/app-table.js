b7c1a4aaef160a2765e387178de34ee7b83e3bd3
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-12-21 16:00:17 +0300

    render goes to net

--- src/applications/main/components/app-table/app-table.js
+++ src/applications/main/components/app-table/app-table.js
@@ -1,19 +1,37 @@
+import { getAllApps } from '../../../../modules/network';
+
 import Element from '../../../element';
 
 import '../app_tile/app_tile';
 
 import './app-table.pcss';
 import template from './app-table.pug';
+import appTemplate from './app.pug';
 
-// import storeApps from './mock';
-
+// import bus from '../../../../modules/bus';
 
 export default class AppTable extends Element {
   constructor(parent, wrapper) {
     super(template, parent, wrapper || parent);
+    // this.wrapper.addEventListener('click', (ev) => {
+    //   if (ev.target !== HTMLImageElement) return;
+    //   const name = ev.target.getAttribute('name');
+
+    // });
   }
 
-  render() {
-    super.render();
+  async render(askedCategory) {
+    let category;
+    if (!askedCategory) category = 'all';
+
+    if (category === 'all') {
+      const { apps, err } = await getAllApps();
+      console.log(apps, err);
+      if (!err) {
+        apps.apps.forEach((app) => {
+          this.wrapper.innerHTML += appTemplate({ app });
+        });
+      }
+    }
   }
 }
