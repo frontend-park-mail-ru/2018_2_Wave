caddb96719bbffef161740fc8867529648319236
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-12-21 15:59:50 +0300

    network methods

--- src/modules/network.js
+++ src/modules/network.js
@@ -1,6 +1,86 @@
 import ajax from './ajax';
 
 
+/*                   apps                   */
+
+async function getAllApps() {
+  try {
+    const apps = await ajax.GET({
+      path: '/apps',
+    });
+    return { apps };
+  } catch (err) {
+    return { err };
+  }
+}
+
+async function getMyApps() {
+  try {
+    const apps = await ajax.GET({
+      path: '/me/apps',
+    });
+    return { apps };
+  } catch (err) {
+    return { err };
+  }
+}
+
+async function addApp(name) {
+  try {
+    return await ajax.POST({
+      path: '/me/apps',
+      body: { name },
+    });
+  } catch (err) {
+    return { err };
+  }
+}
+
+async function getApp(name) {
+  try {
+    const app = await ajax.GET({
+      path: `/me/${name}`,
+    });
+    return { app };
+  } catch (err) {
+    return { err };
+  }
+}
+
+// async function addApp(name) {
+//   try {
+//     return await ajax.POST({
+//       path: '/me/apps',
+//       body: { name },
+//     });
+//   } catch (err) {
+//     return { err };
+//   }
+// }
+
+// /apps GET
+// 200 OK {apps:[{name,cover,description,installations,price,year},...]}
+
+// /me/apps POST {name:'<name of app to add to the profile app list>'}
+// 401 UNAUTHORIZED
+// 200 OK
+
+// /me/apps DELETE {name:'<name of app to delete from the profile app list>'}
+// 401 UNAUTHORIZED
+// 200 OK
+
+// /apps/popular GET
+// 200 OK {apps:[{name,cover,description,installations,price,year},...]}
+
+// /apps/{name} GET
+// 200 OK {name,cover,description,installations,price,year}
+// 404 NOT FOUND
+
+// /me/apps GET
+// 200 OK {name,cover,description,installations,price,year,time_total} !!!!!! <be careful>
+// 401 UNAUTHORIZED
+
+
 /*                  current user                  */
 
 async function register(form) {
@@ -91,11 +171,17 @@ async function logout() {
 
 
 export {
+  getAllApps,
+  getMyApps,
+  addApp,
+  getApp,
+
   register,
   getProfile,
   updateProfile,
   getUser,
   getLeaders,
+
   login,
   logout,
 };
