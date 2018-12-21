2baffcf967950d6b0c2c72b0e521d76cfa40a1fa
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-12-21 06:18:47 +0300

    clock fixed

--- src/applications/main/components/icon-block/icon-block.js
+++ src/applications/main/components/icon-block/icon-block.js
@@ -8,16 +8,19 @@ import './icon-block.pcss';
 export default class IconBlock extends Element {
   constructor(parent, wrapper) {
     super(template, parent, wrapper || parent);
-    this.date = new Date();
     this.render();
+
+    const date = new Date();
+    const timeout = (60 - date.getSeconds()) * 1000;
     setTimeout(() => {
       setInterval(() => this.render(), 60000);
-    }, 60 - this.date.getSeconds());
+    }, timeout);
   }
 
   render() {
-    const hours = this.date.getHours();
-    const minutes = this.date.getMinutes();
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
     const time = `${hours}:${(minutes < 10) ? 0 : ''}${minutes}`;
 
     super.render({ time });
