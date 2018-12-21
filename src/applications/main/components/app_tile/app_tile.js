b351535691473b82ddf9823d3e28d9137bdb5cba
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-12-21 04:23:10 +0300

    app_tile can set additional class

--- src/applications/main/components/app_tile/app_tile.js
+++ src/applications/main/components/app_tile/app_tile.js
@@ -6,10 +6,12 @@ import './app_tile.pcss';
 
 
 export default class AppTile extends Element {
-  constructor(parent, data) {
+  constructor(parent, data, cssClass) {
     super(template, parent);
     this.data = data;
     this.render(data);
+    const [element] = this.wrapper.getElementsByClassName('app__tile');
+    element.classList.add(cssClass);
   }
 
   render(data) {
