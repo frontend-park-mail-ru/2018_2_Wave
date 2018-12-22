bb637906276ff7a060ed28777fe792dd79943732
dlipko <lipko_dima@mail.ru>
2018-12-22 12:20:04 +0300

    profile

--- src/applications/main/components/profile/profile.js
+++ src/applications/main/components/profile/profile.js
@@ -1,6 +1,5 @@
-import userService from '../../../../modules/userservice';
-import bus from '../../../../modules/bus';
 import Element from '../../../element';
+import { updateProfile } from '../../../../modules/network';
 
 import './profile.pcss';
 
@@ -13,38 +12,54 @@ export default class Profile extends Element {
   constructor(parent, wrapper) {
     super(template, parent, wrapper || parent);
 
-    // bus.listen('userUpdated', this.update.bind(this));
-
-    this.avatarWrapper = parent.getElementsByClassName('profile__avatar-wrapper');
-    // this.avatarWrapper.addEventListener('click', async (ev) => {
-    //   console.log(ev.target);
-    //   if (ev.target.classList.contains('add-button')) {
-    //     const { err } = await addApp(this.shownApp.name);
-    //     if (err) console.error(err);
-    //   }
-    // });
+    this.photoUpload = this.photoUpload.bind(this);
   }
 
   update() {
     this.render();
   }
 
-  rende() {
+  photoUpload() {
+    [this.avatarUpload] = document.getElementsByClassName('profile-avatar-upload');
+    this.avatarUpload.click();
+  }
+
+  show() {
+    [this.avatarWrapper] = document.getElementsByClassName('profile__avatar-wrapper');
+    this.avatarWrapper.addEventListener('click', this.photoUpload);
+    super.show();
+    [this.avatarUpload] = document.getElementsByClassName('profile-avatar-upload');
+    this.avatarUpload.addEventListener('change', this.updateAvatar);
+  }
+
+  async updateAvatar() {
+    let form = document.getElementById('uploadForm');
+    const registerData = new FormData(form);
+    console.log(registerData);
+    const { err } = await updateProfile(registerData);
+    if (err) {
+      console.error(err);
+    }
+    console.log('sendAvatar');
+    this.render();
+  }
+
+  render() {
     super.render();
   }
-//   async render() {
-//     const { loggedIn } = userService.isLoggedIn();
+  //   async render() {
+  //     const { loggedIn } = userService.isLoggedIn();
 
-//     if (!loggedIn) {
-//       const user = { username: '' };
-//       super.render({ user, authorized: false });
-//       return;
-//     }
+  //     if (!loggedIn) {
+  //       const user = { username: '' };
+  //       super.render({ user, authorized: false });
+  //       return;
+  //     }
 
 
-//     const authorized = true;
-//     const { user } = userService.getUser();
-//     super.render({ user, authorized });
+  //     const authorized = true;
+  //     const { user } = userService.getUser();
+  //     super.render({ user, authorized });
 
 //     const [profileButton] = this.wrapper.getElementsByClassName('userblock__avatar');
 //     profileButton.addEventListener('click', () => bus.emit('link', '/profile'));
---
+++ src/applications/main/components/profile_discription/profile_description.js
@@ -0,0 +1,60 @@
+import Element from '../../../element';
+
+import bus from '../../../../modules/bus';
+import { getApp, addApp } from '../../../../modules/network';
+
+import template from './profile_description.pug';
+import './profile_description.pcss';
+
+
+export default class ProfileDescription extends Element {
+  constructor(parent, wrapper) {
+    super(template, parent, wrapper || parent);
+
+    bus.listen('profile', this.render.bind(this));
+
+    this.wrapper.addEventListener('click', async (ev) => {
+      console.log(ev.target);
+      if (ev.target.classList.contains('add-button')) {
+        const { err } = await addApp(this.shownApp.name);
+        if (err) console.error(err);
+        else {
+          const element = ev.target;
+          const firstAnimation = element.animate({
+            color: [
+              'rgba(100%, 100%, 100%, 0.8)',
+              'rgba(100%, 100%, 100%, 0)',
+            ],
+          }, {
+            duration: 200,
+            fill: 'forwards',
+            easing: 'cubic-bezier(0.6, 0.04, 0.98, 0.335)',
+          });
+          firstAnimation.pause();
+          firstAnimation.onfinish = () => {
+            firstAnimation.onfinish = null;
+            element.innerText = 'installed';
+            element.classList.remove('add-button');
+            firstAnimation.reverse();
+          };
+          firstAnimation.play();
+        }
+      }
+    });
+
+    this.render();
+  }
+
+  async render(appName) {
+    if (appName) {
+      const { err, app } = await getApp(appName);
+      if (err) console.error(err);
+      this.shownApp = app;
+      super.render({ app });
+      return;
+    }
+
+    this.shownApp = null;
+    super.render();
+  }
+}
