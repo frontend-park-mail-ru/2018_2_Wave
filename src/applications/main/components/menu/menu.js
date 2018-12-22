f09d416b89e8ada9a660c78e721b79b283f97628
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-12-22 12:19:06 +0300

    app tile adds to menu!

--- src/applications/main/components/menu/menu.js
+++ src/applications/main/components/menu/menu.js
@@ -1,10 +1,14 @@
 import Element from '../../../element';
+import { getApp } from '../../../../modules/network';
 
 import '../tile-panel/tile-panel.pcss';
 import '../tile/tile.pcss';
 import './menu.pcss';
 
 import template from './menu.pug';
+import tileTemplate from './tile.pug';
+import appTileTemplate from './menu_app-tile.pug';
+import bus from '../../../../modules/bus';
 
 const tiles = [
   {
@@ -28,6 +32,40 @@ const tiles = [
 export default class Menu extends Element {
   constructor(parent, wrapper) {
     super(template, parent, wrapper);
+    super.render();
+    [this.panel] = this.wrapper.getElementsByClassName('menu__tile-panel');
+    this.render();
+
+    bus.listen('addTile', this.addTile.bind(this));
+  }
+
+  async addTile(appName) {
+    const { err, app } = await getApp(appName);
+    if (err) {
+      console.error(err);
+      return false;
+    }
+
+    const foundTiles = this.panel.querySelectorAll(`[name="${app.name}"]`);
+    const childs = this.panel.childNodes;
+
+    console.log(foundTiles);
+
+    if (childs.length === 4) return true;
+    if (foundTiles.length !== 0) foundTiles[0].remove();
+
+    if (childs.length >= 3) {
+      this.panel.innerHTML += appTileTemplate({ tile: app });
+      return true;
+    }
+    console.log(childs);
+
+    const mock = document.createElement('div');
+    mock.innerHTML = appTileTemplate({ tile: app });
+    const [elem] = mock.getElementsByClassName('menu_tile');
+
+    this.panel.insertBefore(elem, childs[3]);
+    return true;
   }
 
   show() {
@@ -65,6 +103,11 @@ export default class Menu extends Element {
   }
 
   render() {
-    super.render({ tiles });
+    this.panel.innerHTML = '';
+    tiles.forEach((tile) => {
+      this.panel.innerHTML += tileTemplate({ tile });
+      // this.tileList += tile;
+    });
+    console.log('menu rendered');
   }
 }
