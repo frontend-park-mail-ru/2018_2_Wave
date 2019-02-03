f8f1588a9b0ca1dcaf82286fbb66b432d1d4a56b
Dmitry Paliy <dmitrydpaliy@gmail.com>
2019-02-04 00:36:39 +0300

    many keks removed, all except loader and env commented

--- src/applications/main/main_app.js
+++ src/applications/main/main_app.js
@@ -3,62 +3,54 @@ import './styles/fonts.pcss';
 import BaseApp from '../base_app';
 
 import Enviroment from './views/enviroment/env';
-import HomeView from './views/home/home';
-import StoreView from './views/store/store';
+// import HomeView from './views/home/home';
+// import StoreView from './views/store/store';
 import LoaderView from './views/loader/loader';
-import ProfileView from './views/profile/profile';
-import Bar from './components/bar/bar';
-import AppContainer from './views/app_container/app_container';
-import AboutView from './views/about/about';
+// import ProfileView from './views/profile/profile';
+// import Bar from './components/bar/bar';
+// import AppContainer from './views/app_container/app_container';
+// import AboutView from './views/about/about';
 import bus from '../../modules/bus';
-// import LoginView from './views/login';
-// import RegisterView from './views/register';
-// import LeaderboardView from './views/leaderboard';
-// import ProfileView from './views/profile';
-// import SettingsView from './views/settings';
-// import ProfileEditView from './views/editprofile';
+
 
 export default class MenuApp extends BaseApp {
   constructor(appUrl, parent) {
-    // const envWrapper = document.createElement('div');
-    // envWrapper.classList.add('envWrapper');
-    // const env = new Enviroment(parent, envWrapper);
-    const env = new Enviroment(parent, parent);
-    super(appUrl, env.contentPlace);
+    super(appUrl);
+    const env = new Enviroment(parent);
 
-    const [loaderPlace] = env.wrapper.getElementsByClassName('loader-container');
-    this.loader = new LoaderView(loaderPlace, loaderPlace);
-    this.loader.show();
-
-    const [homePlace] = env.wrapper.getElementsByClassName('home-page');
-    this.views.main = new HomeView(homePlace, homePlace);
-    this.currentView = this.views.main;
-
-    const [storePlace] = env.wrapper.getElementsByClassName('store');
-    const storeView = new StoreView(storePlace, storePlace, this.views);
-    this.views.store = storeView;
-
-    const [profilePlace] = env.wrapper.getElementsByClassName('profile');
-    const profileView = new ProfileView(profilePlace, profilePlace, this.views);
-    this.views.profile = profileView;
-
-    const [aboutPlace] = env.wrapper.getElementsByClassName('about');
-    const aboutView = new AboutView(aboutPlace, aboutPlace, this.views);
-    this.views.about = aboutView;
-
-    this.env = env;
-    this.views.env = this.env;
-    this.menu = this.env.menu;
-
-    this.bar = new Bar(
-      this.env.appContainerPlace,
-      this.env.appContainerPlace,
+    this.loaderView = new LoaderView(
+      { parent: env, markTag: 'loader' },
     );
 
-    this.appContainer = new AppContainer(
-      this.env.appContainerPlace,
-      this.env.appContainerPlace,
-    );
+    // const [homePlace] = env.wrapper.getElementsByClassName('home-page');
+    // this.views.main = new HomeView(homePlace, homePlace);
+    // this.currentView = this.views.main;
+
+    // const [storePlace] = env.wrapper.getElementsByClassName('store');
+    // const storeView = new StoreView(storePlace, storePlace, this.views);
+    // this.views.store = storeView;
+
+    // const [profilePlace] = env.wrapper.getElementsByClassName('profile');
+    // const profileView = new ProfileView(profilePlace, profilePlace, this.views);
+    // this.views.profile = profileView;
+
+    // const [aboutPlace] = env.wrapper.getElementsByClassName('about');
+    // const aboutView = new AboutView(aboutPlace, aboutPlace, this.views);
+    // this.views.about = aboutView;
+
+    // this.env = env;
+    // this.views.env = this.env;
+    // this.menu = this.env.menu;
+
+    // this.bar = new Bar(
+    //   this.env.appContainerPlace,
+    //   this.env.appContainerPlace,
+    // );
+
+    // this.appContainer = new AppContainer(
+    //   this.env.appContainerPlace,
+    //   this.env.appContainerPlace,
+    // );
   }
 
   animateLaunch() {
@@ -72,9 +64,6 @@ export default class MenuApp extends BaseApp {
     }, { once: true });
   }
 
-  // start() {
-
-  // }
 
   start() {
     this.appContainer.hide();
