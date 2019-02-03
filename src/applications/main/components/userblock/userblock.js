01e81ba7a1c854153f60a54fbb15b89f87bfbea1
Dmitry Paliy <dmitrydpaliy@gmail.com>
2019-02-03 20:25:31 +0300

    userblock uses component

--- src/applications/main/components/userblock/userblock.js
+++ src/applications/main/components/userblock/userblock.js
@@ -1,27 +1,35 @@
 import userService from '../../../../modules/userservice';
 import bus from '../../../../modules/bus';
-import Element from '../../../element';
+import Component from '../../../component';
 
 import './userblock.pcss';
 
-import '../../../../../static/img/triss.jpg';
+// import '../../../../../static/img/triss.jpg';
 
 
 const template = require('./userblock.pug');
 
 
-export default class UserBlock extends Element {
-  constructor(parent, wrapper) {
-    super(template, parent, wrapper || parent);
+export default class UserBlock extends Component {
+  constructor({ name = 'userblock', parent, markTag = 'userblock' }) {
+    super({
+      name,
+      template,
+      parent,
+      markTag,
+    });
 
     bus.listen('userUpdated', this.update.bind(this));
   }
 
+
+  // TODO: what is this?
   update() {
     this.render();
   }
 
   async render() {
+    // TODO: why not await?
     const { user } = userService.getUser();
     console.log(user);
     super.render({ user });
