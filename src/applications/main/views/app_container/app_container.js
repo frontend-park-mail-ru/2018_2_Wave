c760733887c695f594ea45166532f70ccfa03fb3
Dmitry Paliy <dmitrydpaliy@gmail.com>
2019-02-04 04:31:40 +0300

    appcontainer uses component

--- src/applications/main/views/app_container/app_container.js
+++ src/applications/main/views/app_container/app_container.js
@@ -1,27 +1,11 @@
 import './app_container.pcss';
 
-import Element from '../../../element';
+import Component from '../../../component';
 import template from './app_container.pug';
 
 
-export default class AppContainer extends Element {
-  constructor(parent, wrapper) {
-    super(template, parent, wrapper || parent);
-    this.render();
-    [this.screen] = this.wrapper.getElementsByClassName('screen');
-  }
-
-  hide() {
-    this.wrapper.style['z-index'] = -5;
-    super.hide();
-  }
-
-  show() {
-    super.show();
-    this.wrapper.style['z-index'] = 2;
-  }
-
-  render() {
-    this.wrapper.innerHTML += this.template();
+export default class AppContainer extends Component {
+  constructor({ parent, markTag }) {
+    super({ template, parent, markTag });
   }
 }
