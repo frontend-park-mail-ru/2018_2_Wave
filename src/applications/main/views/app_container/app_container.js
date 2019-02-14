10b60f4d67347c250f4328241e2a26cce191c0e9
Dmitry Paliy <d.paliy@corp.mail.ru>
2019-02-14 08:29:59 +0300

    jsdoc to AppContainer

--- src/applications/main/views/app_container/app_container.js
+++ src/applications/main/views/app_container/app_container.js
@@ -3,7 +3,10 @@ import './app_container.pcss';
 import Component from '../../../component';
 import template from './app_container.pug';
 
-
+/**
+ * Wrapper contains all non-main apps
+ * @extends Component
+ */
 export default class AppContainer extends Component {
   constructor({ parent, markTag }) {
     super({ template, parent, markTag });
