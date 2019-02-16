c7f8e84c91bdf3e05fb7cfab7031aa38cf0b5213
Dmitry Paliy <d.paliy@corp.mail.ru>
2019-02-16 08:01:32 +0300

    jsdocs, hideActiveApp, opening mainApp

--- src/modules/app_manager.js
+++ src/modules/app_manager.js
@@ -1,3 +1,5 @@
+import bus from './bus';
+
 class AppManager {
   constructor() {
     this.appClasses = {};
@@ -21,8 +23,10 @@ class AppManager {
     this.appContainer = this.mainApp.appContainer;
 
     this.activeApp = this.mainApp;
+    this.activeAppName = 'main';
     this.started = true;
 
+    bus.listen('regApp', this.registerApp.bind(this));
     return this;
   }
 
@@ -50,8 +54,12 @@ class AppManager {
     return this;
   }
 
+  /**
+   * Checks, if app is known by AppManager, or is MainApp.
+   * @param {String} appName
+   */
   appExists(appName) {
-    return appName in this.appClasses;
+    return (appName in this.appClasses) || (appName === 'main');
   }
 
 
@@ -60,45 +68,44 @@ class AppManager {
       throw new Error('App not exists');
     }
 
-    if (!(appName in this.appInstances)) {
+    if (!(appName in this.appInstances) && appName !== 'main') {
       const App = this.appClasses[appName];
 
       this.appInstances[appName] = new App(
-        appName,
-        this.appContainer.screen,
+        appName, this.appContainer.screen,
       );
     }
 
-    const app = this.appInstances[appName];
+    const app = this.appInstances[appName] || this.mainApp;
 
-    if (params) {
-      app.processParams(...params);
-    }
+    if (params) app.processParams(...params);
 
-    if (app !== this.activeApp) { // TODO: // FIXME hide last app
+    if (app !== this.activeApp) {
+      await this.hideActiveApp();
+      this.activeApp = app;
+      this.activeAppName = appName;
       // await launch animation
       // async show bar for 3 seconds
-      if (!app.started) {
+      if (!app.started) {  // CHECK: mainApp.started?
         // show loader
         /* await */ app.start();
         // hide loader
       } else {
-        app.resume();
+        app.resume();  // CHECK: mainApp.resume? unblur there
       }
     }
 
     this._addToActive_(appName);
   }
 
-  async hideApp(appName) {
-    if (!this.appExists(appName)) {
-      throw new Error('App not exists');
-    }
 
-    // await hide animation
-
-    this.appInstances[appName].pause();
+  async hideActiveApp() {
+    this.activeApp.pause();  // CHECK: mainApp.pause? blur there
+    // TODO: hide bar
+    // CHECK: is animation needed?
+    // CHECK: is app hidden?
   }
+
 
   async closeApp(appName) {
     if (!this.appExists(appName)) {
