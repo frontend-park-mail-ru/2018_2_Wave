0cdabc2e1951c15bbcf4f4f43d8aab3c43cc32de
Dmitry Paliy <d.paliy@corp.mail.ru>
2019-02-21 00:46:57 +0300

    style fixes

--- src/app.js
+++ src/app.js
@@ -1,21 +1,22 @@
+import '../static/favicon.ico';
+import './style.pcss';
+
+import bus from './modules/bus';
 import appManager from './modules/app_manager';
+
 import Router from './modules/router';
 import Loader from './applications/loader/loader';
 import MainApp from './applications/main/main_app';
-import Terminal from './applications/terminal/terminal_app';
-import Snake from './applications/snake/game_app';
+// import Terminal from './applications/terminal/terminal_app';
+// import Snake from './applications/snake/game_app';
 
-import bus from './modules/bus';
-
-import '../static/favicon.ico';
-import './style.pcss';
 
 new Loader(document.body)
   .start();
 
 appManager
-  .registerApp('terminal', Terminal)
-  .registerApp('snake', Snake)
+  // .registerApp('terminal', Terminal)
+  // .registerApp('snake', Snake)
   .start(MainApp, document.body)
   .then(new Router(appManager).start)
   .then(() => bus.emit('loaded'));
