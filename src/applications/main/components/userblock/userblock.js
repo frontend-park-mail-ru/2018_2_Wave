c61df271fa4c370ff636204b12ee342e9e0c1335
Dmitry Paliy <d.paliy@corp.mail.ru>
2019-02-21 00:47:35 +0300

    userblock upd

--- src/applications/main/components/userblock/userblock.js
+++ src/applications/main/components/userblock/userblock.js
@@ -14,28 +14,14 @@ export default class UserBlock extends Component {
   constructor(parent, markTag = 'userblock') {
     super({ template, parent, markTag });
 
-    bus.listen('userUpdated', this.update.bind(this));
-  }
-
-
-  // TODO // FIXME what is this?
-  update() {
-    this.render();
+    bus.listen('userUpdated', super.render.bind(this));
   }
 
   /* eslint-disable class-methods-use-this */
   async getData() {
     const { err, user } = await userService.getUser();
-
     if (err) return { user: { username: 'not logged in' } };
 
     return { user };
   }
-
-  // async render() {
-  //   // TODO: why not await?
-  //   const { user } = userService.getUser();
-  //   console.log(user);
-  //   super.render({ user });
-  // }
 }
