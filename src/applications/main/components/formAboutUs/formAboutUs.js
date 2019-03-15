805833c056649a3148f333ac49a28fac677abd35
Dmitry Paliy <d.paliy@corp.mail.ru>
2019-03-15 12:52:12 +0300

    about us form upd

--- src/applications/main/components/formAboutUs/formAboutUs.js
+++ src/applications/main/components/formAboutUs/formAboutUs.js
@@ -1,6 +1,8 @@
 import Element from '../../../element';
 
 import bus from '../../../../modules/bus';
+import localeManager from '../../../../modules/locale';
+
 // import { getApp, addApp } from '../../../../modules/network';
 
 import template from './formAboutUs.pug';
@@ -11,20 +13,24 @@ export default class FormAboutUs extends Element {
   constructor(parent, wrapper) {
     super(template, parent, wrapper || parent);
 
-    bus.listen('about', this.render.bind(this));
+    this.render = this.render.bind(this);
+
+    bus.listen('about', this.render);
+    bus.listen('localeChanged', this.render);
+
     this.render();
   }
 
-  render(appName) {
-    // if (appName) {
-    //   const { err, app } = await getApp(appName);
-    //   if (err) console.error(err);
-    //   this.shownApp = app;
-    //   super.render({ app });
-    //   return;
-    // }
+  render() {
+    let text = 'Place for your feedback and suggestions for new games';
+    const { locale } = localeManager;
 
-    // this.shownApp = null;
-    super.render();
+    if (locale === 'RU') {
+      text = 'Здесь вы можете отправить отзыв или предложить новую игру для платформы';
+    } else if (locale === 'DE') {
+      text = 'Hier können Sie Feedback senden oder ein neues Spiel für die Plattform vorschlagen';
+    }
+
+    super.render({ text });
   }
 }
