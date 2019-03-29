bb973801971bade6322e6926f277b7dfa204a67f
Dmitry Paliy <d.paliy@corp.mail.ru>
2019-03-29 21:45:16 +0300

    postcss-preset-env instead of deprecated cssnext

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: [
     require('postcss-import'),
     require('precss'),
-    require('postcss-cssnext'),
+    require('postcss-preset-env'),
     require('postcss-google-font'),
     // require('cssnano'),
   ],
