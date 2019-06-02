5c7ac3e0bf2599716e068f0683f0cd90c61d762c
Dmitry Paliy <d.paliy@corp.mail.ru>
2019-06-02 08:18:45 +0300

    appTable extends Component

--- src/applications/main/components/app-table/app-table.js
+++ src/applications/main/components/app-table/app-table.js
@@ -1,8 +1,8 @@
 import { getAllApps } from '../../../../modules/network';
 
-import Element from '../../../element';
+import Component from '../../../component';
 
-import '../app_tile/app_tile';
+// import '../app_tile/app_tile';
 
 import './app-table.pcss';
 import template from './app-table.pug';
@@ -10,30 +10,30 @@ import appTemplate from './app.pug';
 
 import bus from '../../../../modules/bus';
 
-export default class AppTable extends Element {
-  constructor(parent, wrapper) {
-    super(template, parent, wrapper || parent);
-    this.wrapper.addEventListener('click', (ev) => {
-      if (!(ev.target instanceof HTMLImageElement)) return;
-      const name = ev.target.getAttribute('name');
-      console.log('emitted');
-      bus.emit('about', name);
-    });
+export default class AppTable extends Component {
+  constructor(parent, markTag = 'AppTable') {
+    super({ template, parent, markTag });
+    // this.wrapper.addEventListener('click', (ev) => {
+    //   if (!(ev.target instanceof HTMLImageElement)) return;
+    //   const name = ev.target.getAttribute('name');
+    //   console.log('emitted');
+    //   bus.emit('about', name);
+    // });
   }
 
-  async render(askedCategory) {
+  async getData() {
     console.log('rendering matrix');
-    let category;
-    if (!askedCategory) category = 'all';
 
-    if (category === 'all') {
-      const { apps, err } = await getAllApps();
-      console.log(apps, err);
-      if (!err) {
-        apps.apps.forEach((app) => {
-          this.wrapper.innerHTML += appTemplate({ app });
-        });
-      }
-    }
+    const { apps, err } = await getAllApps();
+    console.log(apps, err);
+    return { apps };
   }
+
+  // async render() {
+  //   if (!err) {
+  //     apps.apps.forEach((app) => {
+  //       this.wrapper.innerHTML += appTemplate({ app });
+  //     });
+  //   }
+  // }
 }
