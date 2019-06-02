d950d8a0c6197dbe2438403f982646242d8bb4eb
Dmitry Paliy <d.paliy@corp.mail.ru>
2019-06-02 08:20:06 +0300

    store redisign begin

--- src/applications/main/views/store/store.js
+++ src/applications/main/views/store/store.js
@@ -1,6 +1,6 @@
-import Element from '../../../element';
-import List from '../../components/list/list';
-import Description from '../../components/description/description';
+import Component from '../../../component';
+// import List from '../../components/list/list';
+// import Description from '../../components/description/description';
 import AppTable from '../../components/app-table/app-table';
 
 import template from './store.pug';
@@ -18,53 +18,51 @@ const categories = [
   '2017_2',
 ];
 
-export default class StoreView extends Element {
-  constructor(parent, wrapper, views) {
-    super(template, parent, wrapper);
-    super.render();
+export default class StoreView extends Component {
+  constructor(parent, markTag = 'store') {
+    super({ template, parent, markTag });
 
     this.title = 'Store';
 
-    this.parentViews = views;
-    [this.panel] = this.wrapper.getElementsByClassName('store__tile-panel');
-    [this.listPlace] = document.getElementsByClassName('list');
-    [this.descriptionPlace] = document.getElementsByClassName('description-container');
-    [this.appTablePlace] = document.getElementsByClassName('app-table');
+    // [this.panel] = this.wrapper.getElementsByClassName('store__tile-panel');
+    // [this.listPlace] = document.getElementsByClassName('list');
+    // [this.descriptionPlace] = document.getElementsByClassName('description-container');
+    // [this.appTablePlace] = document.getElementsByClassName('app-table');
 
-    this.appTable = new AppTable(this.appTablePlace, this.appTablePlace);
-    this.description = new Description(this.descriptionPlace);
-    this.list = new List(this.listPlace, this.listPlace);
-    this.listPlace.addEventListener('click', (event) => {
-      const categoryName = event.target.innerText.toLowerCase();
-      if (categoryName) this.render(categoryName);
-    });
+    this.appTable = new AppTable(this, 'app-table');
+    // this.description = new Description(this.descriptionPlace);
+    // this.list = new List(this.listPlace, this.listPlace);
+    // this.listPlace.addEventListener('click', (event) => {
+    //   const categoryName = event.target.innerText.toLowerCase();
+    //   if (categoryName) this.render(categoryName);
+    // });
 
-    this.list.render(categories);
+    // this.list.render(categories);
   }
 
-  show() {
-    const [grid] = document.getElementsByClassName('grid-common');
-    if (!grid.classList.contains('store__grid')) {
-      grid.classList.add('store__grid');
-    }
+  // show() {
+  //   const [grid] = document.getElementsByClassName('grid-common');
+  //   if (!grid.classList.contains('store__grid')) {
+  //     grid.classList.add('store__grid');
+  //   }
 
-    this.appTable.show();
-    this.description.show();
-    this.list.show();
-    super.show();
-    // this.parentViews.env.menu.hide();
-  }
+  //   this.appTable.show();
+  //   // this.description.show();
+  //   // this.list.show();
+  //   super.show();
+  //   // this.parentViews.env.menu.hide();
+  // }
 
-  hide() {
-    const [grid] = document.getElementsByClassName('grid-common');
-    if (grid.classList.contains('store__grid')) {
-      grid.classList.remove('store__grid');
-    }
+  // hide() {
+  //   const [grid] = document.getElementsByClassName('grid-common');
+  //   if (grid.classList.contains('store__grid')) {
+  //     grid.classList.remove('store__grid');
+  //   }
 
-    this.appTable.hide();
-    this.description.hide();
-    this.list.hide();
-    super.hide();
-    // this.parentViews.env.menu.show();
-  }
+  //   this.appTable.hide();
+  //   // this.description.hide();
+  //   // this.list.hide();
+  //   super.hide();
+  //   // this.parentViews.env.menu.show();
+  // }
 }
