0d3db4cb4c0792c3c0528c13b91524c2cedc65bc
Dmitry Paliy <d.paliy@corp.mail.ru>
2019-06-02 08:26:49 +0300

    animation methods muted

--- src/applications/base_app.js
+++ src/applications/base_app.js
@@ -13,7 +13,6 @@ export default class BaseApp {
     this.started = false;
   }
 
-
   // TODO: write this.
   processParams() {
     console.log('processing params');
@@ -21,90 +20,73 @@ export default class BaseApp {
   }
 
 
-  launch(resource) {
-    if (resource) this.animateLaunch(resource);
-    else if (this.started) this.resume();
-    else this.start();
-  }
+  // launch(resource) {
+  //   if (resource) this.animateLaunch(resource);
+  //   else if (this.started) this.resume();
+  //   else this.start();
+  // }
 
-  animateLaunch(resource) {
-    const {
-      width,
-      height,
-      left,
-      top,
-    } = resource.getBoundingClientRect();
+  // animateLaunch(resource) {
+  //   const {
+  //     width,
+  //     height,
+  //     left,
+  //     top,
+  //   } = resource.getBoundingClientRect();
 
-    const element = document.createElement('div');
-    element.classList.add('floating');
-    element.style.width = `${width}px`;
-    element.style.height = `${height}px`;
-    element.style.top = `${top}px`;
-    element.style.left = `${left}px`;
+  //   const element = document.createElement('div');
+  //   element.classList.add('floating');
+  //   element.style.width = `${width}px`;
+  //   element.style.height = `${height}px`;
+  //   element.style.top = `${top}px`;
+  //   element.style.left = `${left}px`;
 
-    this.parent.appendChild(element);
-    const widthScale = window.innerWidth / width;
-    const heightScale = window.innerHeight / height;
-    const translateX = (window.innerWidth / 2 - left - width / 2) / widthScale;
-    const translateY = (window.innerHeight / 2 - top - height / 2) / heightScale;
+  //   this.parent.appendChild(element);
+  //   const widthScale = window.innerWidth / width;
+  //   const heightScale = window.innerHeight / height;
+  //   const translateX = (window.innerWidth / 2 - left - width / 2) / widthScale;
+  //   const translateY = (window.innerHeight / 2 - top - height / 2) / heightScale;
 
-    const launchAnimation = element.animate({
-      transform: [
-        'translate(0px, 0px)',
-        `scale(${widthScale}, ${heightScale}) translate(${translateX}px, ${translateY}px)`,
-      ],
-      'border-radius': [
-        '20px', '0px',
-      ],
-    }, {
-      duration: 300,
-      fill: 'forwards',
-      easing: 'cubic-bezier(.36,1.08,.55,.93)',
-    });
-    launchAnimation.pause();
+  //   const launchAnimation = element.animate({
+  //     transform: [
+  //       'translate(0px, 0px)',
+  //       `scale(${widthScale}, ${heightScale}) translate(${translateX}px, ${translateY}px)`,
+  //     ],
+  //     'border-radius': [
+  //       '20px', '0px',
+  //     ],
+  //   }, {
+  //     duration: 300,
+  //     fill: 'forwards',
+  //     easing: 'cubic-bezier(.36,1.08,.55,.93)',
+  //   });
+  //   launchAnimation.pause();
 
-    launchAnimation.onfinish = () => {
-      element.remove();
-      if (this.started) this.resume();
-      else this.start();
-    };
+  //   launchAnimation.onfinish = () => {
+  //     element.remove();
+  //     if (this.started) this.resume();
+  //     else this.start();
+  //   };
 
-    launchAnimation.play();
+  //   launchAnimation.play();
 
-    const name = resource.getAttribute('name');
-    console.log(name);
-    setTimeout(() => bus.emit('addTile', name), 1000);
-  }
+  //   const name = resource.getAttribute('name');
+  //   console.log(name);
+  //   setTimeout(() => bus.emit('addTile', name), 1000);
+  // }
 
   start() {
     this.started = true;
     this.active = true;
-    this.currentView.show();
-
-    // this.bar.show();
   }
 
   stop() {
     this.started = false;
     this.active = false;
-    this.currentView.hide();
-    this.parent.innerHTML = '';
-    // this.bar.hide();
   }
 
   pause() {
-    this.parent.style.background = 'none';
     this.active = false;
-    // const barAnimation = this.bar.hide();
-    // if (barAnimation) {
-    //   barAnimation.pause();
-    //   barAnimation.onfinish = () => {
-    //     this.currentView.hide();
-    //   };
-    //   barAnimation.play();
-    // } else {
-    //   this.currentView.hide();
-    // }
   }
 
   resume() {
@@ -112,8 +94,7 @@ export default class BaseApp {
       this.start();
       return;
     }
-    this.bar.show();
-    this.currentView.show();
+
     this.active = true;
   }
 }
